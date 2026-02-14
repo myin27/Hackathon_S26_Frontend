8896707c7671b4d5f5678df00a6f16050ee9c0b6
Madison Yin <madisontyin@gmail.com>
2026-02-14 12:20:39 -0500

    move script from html to js file

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // script.js
+
+const input = document.getElementById("fileUpload");
+        const preview = document.getElementById("preview");
+    
+        input.addEventListener("change", () => {
+        const file = input.files[0];
+        if (file) {
+            preview.src = URL.createObjectURL(file);
+            preview.style.display = "block";
+        }
+});
